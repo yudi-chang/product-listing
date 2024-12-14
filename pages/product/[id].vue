c4361151fbeb1c86d0a154dc09e2370aba6f2e73
Yudi Chang <m.yudi.chang@gmail.com>
2024-12-14 15:52:03 +0700

    fix and update carousel type

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -7,29 +7,7 @@
     <Container v-else-if="productStore.product">
       <div class="product-wrapper">
         <div class="product-images-wrapper">
-          <swiper
-            :style="{
-              '--swiper-navigation-color': '#fff',
-              '--swiper-pagination-color': '#fff',
-            }"
-            :spaceBetween="10"
-            :thumbs="{ swiper: thumbsSwiper }"
-            :modules="modules"
-            class="mySwiper2"
-          >
-            <swiper-slide v-for="(image, index) in productStore.product.images" :key="index">
-              <img :src="image" alt="Product Image" />
-            </swiper-slide>
-          </swiper>
-          <swiper
-            @swiper="setThumbsSwiper"
-            :spaceBetween="10"
-            :slidesPerView="4"
-            :freeMode="true"
-            :watchSlidesProgress="true"
-            :modules="modules"
-            class="mySwiper"
-          >
+          <swiper :pagination="true" :modules="modules" :spaceBetween="15" class="mySwiper">
             <swiper-slide v-for="(image, index) in productStore.product.images" :key="index">
               <img :src="image" alt="Product Image" />
             </swiper-slide>
@@ -78,11 +56,9 @@ import { onMounted } from 'vue';
 import { useProductStore } from '~/stores/product';
 import { useRoute } from 'vue-router';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
+import 'swiper/css/pagination';
 
 // store
 const route = useRoute();
@@ -93,7 +69,7 @@ const selectedVariation = ref(null);
 const thumbsSwiper = ref(null);
 
 // others
-const modules = [FreeMode, Navigation, Thumbs];
+const modules = [Pagination];
 
 // cycle
 onMounted(() => {
@@ -180,53 +156,21 @@ const setThumbsSwiper = (swiper) => {
 .swiper {
   width: 100%;
   height: 100%;
-  
-  &-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      display: block;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-
-    &.swiper-slide-thumb-active {
-      opacity: 1;
-    }
-  }
-
-  &-slide {
-    background-size: cover;
-    background-position: center;
-  }
-
-  &.mySwiper2 {
-    height: 80%;
-    width: 100%;
-
-    .swiper-slide {
-      cursor: grab;
-    }
-  }
-
-  &.mySwiper {
-    height: 20%;
-    box-sizing: border-box;
-    padding: 10px 0;
-    
-    .swiper-slide {
-      width: 25%;
-      height: 100%;
-      opacity: 0.4;
-      cursor: pointer;
-    }
-  }
 }
 
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
 </style>
